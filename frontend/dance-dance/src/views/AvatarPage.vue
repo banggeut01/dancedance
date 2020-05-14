4fc6498be5a85149d0ed40b6ff3d0c75c59cba96
김준영 <kimjy392@gmail.com>
2020-05-14 16:53:51 +0900

    Add | frontend skeleton code

---
+++ frontend/dance-dance/src/views/AvatarPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    avatar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatarPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/dance-dance/src/views/ResultPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    result
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'ResultPage'
+}
+</script>
+
+<style>
+
+</style>
