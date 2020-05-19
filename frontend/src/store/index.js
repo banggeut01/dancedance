f8986a7dc377d32975fe554351eb196876a6459b
kimjunyoung <kimjy392@gmail.com>
2020-05-19 14:49:35 +0900

    S02P31B102-31 | Add global axios, loginForm

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,10 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token : null,
+    user : {}
   },
   mutations: {
+    getUserInfo(state, payload) {
+      state.token = payload.token
+      state.user = payload.user
+    },
+    deleteUserInfo(state) {
+      state.token = null
+      state.user = {}
+    }
   },
   actions: {
+    
   },
   modules: {
   }
