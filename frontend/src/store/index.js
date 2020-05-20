49de195d751fa359d891bfd4ff69b9de26d3ffbc
kimjunyoung <kimjy392@gmail.com>
2020-05-20 11:30:25 +0900

    S02P31B102-35 | Add request login

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    host : 'http://localhost:8197/ssafy-dance/api',
     token : null,
     user : {}
   },
@@ -19,7 +20,19 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    
+    //세션에 저장하기 및 로그인 상태인지 확인하기
+  },
+  getters : {
+    // options(state) {
+    //     return{
+    //         headers: {
+    //             Authorization : `JWT ${state.token}` // JWT 다음에 공백있음!
+    //         }
+    //     }        
+    // },
+    // user(state) {
+    //     return jwtDecode(state.token).user_id
+    // }
   },
   modules: {
   }
