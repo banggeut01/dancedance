75192121515dab825e2bfa1e28f7f6f9ad0ae6d0
yeomkyeorae <duarufp06@gmail.com>
2020-05-22 11:31:19 +0900

    S02P31B102-58 | add tmpFaceMesh page

---
+++ frontend/src/views/TmpFaceMeshPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <FaceMesh />
+</template>
+
+<script>
+import FaceMesh from '../components/faceMesh/FaceMesh.vue'
+
+export default {
+    name: 'TmpFaceMeshPage',
+    components: {
+        FaceMesh
+    }
+}
+</script>
+
+<style>
+
+</style>
