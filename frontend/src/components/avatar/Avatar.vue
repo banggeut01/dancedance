3fcdacbff4ec8c3fb27bc223d701901b937afa45
yeomkyeorae <duarufp06@gmail.com>
2020-05-22 16:11:43 +0900

    S02P31B102 | apply svg image files & more fancy avatar layer

--- frontend/src/components/avatar/Avatar.vue
+++ frontend/src/components/avatar/Avatar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="imgDiv" :class="{selectedGlow: selected, unselectedGlow: !selected}">
         <img :src="require(`${avatar.url}`)" class="avatarImg">
         <h3 style="color:white; text-align:center">{{ avatar.name }}</h3>
     </div>
@@ -11,6 +11,9 @@
         props: {
             avatar: {
                 type: Object
+            },
+            selected: {
+                type: Boolean
             }
         },
     }
@@ -18,7 +21,45 @@
 
 <style>
     .avatarImg {
-        max-width: 170px;
-        max-height: 130px;
+        width: 210px;
+        height: 200px;
+        /* max-width: 170px;
+        max-height: 130px; */
+    }
+
+    .imgDiv {
+        margin-top: 10px;
+        margin-left: 10px;
+        border-radius: 10px;
+    }
+
+    .selectedGlow {
+        color: #fff;
+        box-shadow: 0 0 2px #fff, 0 0 10px #fff, 0 0 20px gold, 0 0 30px gold,
+            0 0 40px gold, 0 0 50px gold;
+        -webkit-animation: blink 0.5s infinite alternate;
+        animation: blink 0.5s infinite alternate;
+    }
+
+    .unselectedGlow {
+        color: #fff;
+        box-shadow: 0 0 2px #fff, 0 0 10px #fff, 0 0 20px #9F00C5, 0 0 30px #9F00C5,
+            0 0 40px #9F00C5, 0 0 50px #9F00C5;
+        -webkit-animation: blink 0.5s infinite alternate;
+        animation: blink 0.5s infinite alternate;
+    }
+
+    @-webkit-keyframes blink {
+        100% {
+            box-shadow: 0 0 3px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #0017A8,
+                0 0 70px #0017A8, 0 0 80px #0017A8;
+        }
+    }
+
+    @keyframes blink {
+        100% {
+            box-shadow: 0 0 3px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #0017A8,
+                0 0 70px #0017A8, 0 0 80px #0017A8;
+        }
     }
 </style>
