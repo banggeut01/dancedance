41f9be75e110fa552d5c53362b6643acb19615db
banggeut01 <genie121110@gmail.com>
2020-05-26 14:46:18 +0900

    S02P31B102-80 | Add Ranking

--- frontend/src/views/RankPage.vue
+++ frontend/src/views/RankPage.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
     rank
+    <v-carousel hide-delimiters>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
   </div> 
 </template>
 
 <script>
 export default {
-  name: 'RankPage'
+  name: 'RankPage',
+  data () {
+      return {
+        items: [
+          {
+            src: require('@/assets/stage.png'),
+          },
+          {
+            src: require('@/assets/stage.png'),
+          },
+          {
+            src: require('@/assets/stage.png'),
+          },
+          {
+            src: require('@/assets/stage.png'),
+          },
+        ],
+      }
+    },
 }
 </script>
 
