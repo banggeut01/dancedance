d2e223eecf39f51206d6a59eebf0aae2419b2324
kimjunyoung <kimjy392@gmail.com>
2020-05-27 14:28:19 +0900

    S02P31B102-66 | Add introPage skeleton

--- frontend/src/views/IntroPage.vue
+++ frontend/src/views/IntroPage.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
      <div class="intro">
-      <h1 id="test1">The New Dev Ed Pro</h1>
-      <video ref="vedio" src="../assets/intro1.mp4"></video>
+      <h1 id="text1">아무 말이나 적어야지</h1>
+      <h1 id="text2">아무말 아무말</h1>
+      <img id="myimg">
     </div>
     <section class="test">
       <h1>REVOLUTIONARRY</h1>
@@ -11,46 +12,97 @@
 </template>
 
 <script>
-import { TweenMax, TimelineMax } from 'gsap'
 
 export default {
   name: 'IntroPage',
+  data() {
+    return {
+
+    }
+  },
   methods: {
     scrollmagic() {
+      // img
+      const img = document.querySelector('#myimg')
+      const imageArray = new Array;
+      this.preloading(79, imageArray)
+      console.log(imageArray)
+      let obj = {curImg: 1};
+      let maxlength = imageArray.length - 1;
+      let tween = TweenLite.to(obj, 1, 
+          {
+              curImg: maxlength,
+              roundProps : 'curImg',
+              immediateRender: true,
+              ease: Linear.easeNone,
+              onUpdate: function () {
+                  img.src = imageArray[obj.curImg * 1].src
+              }
+          }   
+      );
+      // intro pin set
       const scene = this.$scrollmagic.scene({
-        duration: '6000',
+        duration: '5000',
         triggerElement: '.intro',
         triggerHook: '0'
       })
       .setPin('.intro')
+      .setTween(tween)
       .addIndicators()
 
-
+      
+      //text1
       let scene2 = this.$scrollmagic.scene({
-        duration: '2000',
+        duration: '1000',
         triggerElement: '.intro',
         triggerHook: '0'
-      }).setTween(TweenLite.fromTo('#test1', 2, 
+      }).setTween(TweenLite.fromTo('#text1', 2, 
       {opacity: 1},
       {opacity: 0}
       ))
       .addIndicators()
+
+      //text2
+      const text2 = document.querySelector('#text2')
+      let tl = new TimelineMax();
+      tl.from(text2, 1, { opacity:0, y:200});
+      tl.to(text2, 1, { opacity:0 ,y:-200}, 3);
+      let scene3 = this.$scrollmagic.scene({
+        duration: '1000',
+        offset: '1500',
+        triggerElement: '.intro',
+        triggerHook: 0
+      })
+      .setTween(tl)
+      .addIndicators()
       this.$scrollmagic.addScene(scene2)
       this.$scrollmagic.addScene(scene)
-      let accelamount = 0.1;
-      let scrollpos = 0;
-      let delay = 0;
-      let video = document.querySelector('video')
-      scene.on("update", e => {
-        scrollpos = e.scrollPos / 1000;
-      });
+      this.$scrollmagic.addScene(scene3)
+      // let accelamount = 0.1;
+      // let scrollpos = 0;
+      // let delay = 0;
+      // let video = document.querySelector('video')
+      // scene.on("update", e => {
+      //   scrollpos = e.scrollPos / 1000;
+      // });
 
-      setInterval(() => {
-        delay += (scrollpos - delay) * accelamount;
+      // setInterval(() => {
+      //   delay += (scrollpos - delay) * accelamount;
 
-        video.currentTime = delay;
-      }, 30);
-    }
+      //   video.currentTime = delay;
+      // }, 30);
+    },
+    pad(n, width) {
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+    },
+    preloading(n, imageArray) {
+      for (let i=1; i <= n; i++) {
+        let img = new Image();
+        img.src = require(`../assets/introImg/scene${this.pad(i, 5)}.jpg`)
+        imageArray.push(img)
+      }
+    } 
   },
   mounted () {
     this.scrollmagic()
@@ -63,7 +115,7 @@ export default {
   height: 100vh;
 }
 
-.intro video {
+.intro img {
   height: 100%;
   width: 100%;
   object-fit: cover;
