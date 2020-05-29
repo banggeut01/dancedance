bf0d2e3bd3ee247988d0b4630e1f947be998f511
banggeut01 <genie121110@gmail.com>
2020-05-29 17:23:04 +0900

    S02P31B102-99 | Add Title

--- frontend/src/components/rank/TotalRank.vue
+++ frontend/src/components/rank/TotalRank.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      TotalRank
+    <div class="totalrank-title">
+      <p class="rank-neon-h1">RANKING</p>
+    </div>
+      
   </div>
 </template>
 
@@ -11,5 +14,14 @@
 </script>
 
 <style>
-
+.totalrank-title {
+  padding: 10px 0px 10px 0;
+}
+.rank-neon-h1{
+  color: #ffffff;;
+  font-family: Iceland;
+  text-align: center;
+  font-size: 3em;
+  text-shadow: 0 0 10px #fff, 0 0 20px #228DFF, 0 0 30px #228DFF, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
+}
 </style>
