7475d08587b8e8fec967dad777d6c2ebce86e41a
yeomkyeorae <duarufp06@gmail.com>
2020-06-01 15:48:23 +0900

    S02P31B102-106 | add buttons

--- frontend/src/views/ResultPage.vue
+++ frontend/src/views/ResultPage.vue
@@ -5,15 +5,19 @@
     </video>
     <div class="resultDiv">
       <div class="resultUpper">
-        <div class="profileImg">
+        <div class="profileDiv">
           <img :src="require('@/assets/profileImg/owl.jpg')" alt="" class="profileImg">
         </div>
         <div class="userInfo">
-          <span>
-            moomoo
+          <span id="userName" class="glow">
+            무무초이<br>
+          </span>
+          <span id="performedTime">
+            2020년 06월 01일 14시 19분
           </span>
         </div>
       </div>
+      <div style="height:20px"></div>
       <div class="resultBottom">
         <div class="starRatings">
           <div class="starRatingsTop"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
@@ -21,17 +25,22 @@
           </div>
         </div>
         <div class="scoreRatings">
-          <span>Score</span>
-          <span>12500pt</span>
+          <span style="font-size:1rem; margin-top:10px">Score</span>
+          <span style="font-size:4rem; margin-top:10px" id="point" class="glow">12400</span>pt
+        </div>
+        <div class="rankingRatings">
+          <span style="color: white; font-size:2rem;">
+            <span id="ranking" style="font-size:4rem; margin-top:10px" class="glow">35</span>th
+          </span>
         </div>
       </div>
-      <div class="resultBtn">
-        <div style="display:inline-block">
-          <button style="color:white;">다시하기</button>
-        </div>
-        <div style="display:inline-block">
-          <button style="color:white; display:inline-block">종료하기</button>
-        </div>
+      <div class="resultBtn" style="text-align:center">
+        <p class="buttonP" style="margin-right:100px;display:inline-block;">
+          <a class="buttonCommon buttonA">다시하기</a>
+        </p>
+        <p class="buttonP" style="margin:0;display:inline;">
+          <a class="buttonCommon buttonB">종료하기</a>
+        </p>
       </div>
     </div>
   </div>
@@ -39,11 +48,100 @@
 
 <script>
   export default {
-    name: 'ResultPage'
+    name: 'ResultPage',
+    data() {
+      return {
+        chgNumSpeed: 0.1
+      }
+    },
+    methods: {
+      increaseNum(id, unit) {
+        var elt = document.getElementById(id);
+        var endNbr = Number(document.getElementById(id).innerHTML);
+        this.incNumRecur(0, endNbr, elt, unit);
+      },
+      /*A recursive function to increase the number.*/
+      incNumRecur(i, endNbr, elt, unit) {
+        if (i <= endNbr) {
+          elt.innerHTML = i;
+          var th = this
+          setTimeout(function () { //Delay a bit before calling the function again.
+            th.incNumRecur(i + unit, endNbr, elt, unit);
+          }, th.chgNumSpeed);
+        }
+      },
+    },
+    mounted() {
+      this.increaseNum("ranking", 1);
+      this.increaseNum("point", 100);
+    }
   }
 </script>
 
 <style>
+  .buttonP {
+    text-align: center;
+    font-size: 3rem;
+    margin: 20px 0 20px 0;
+    cursor: pointer;
+  }
+
+  .buttonCommon {
+    text-decoration: none;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    transition: all 0.5s;
+    font-family: Monoton;
+  }
+
+  .buttonA {
+    color: #fff;
+      -webkit-animation: neon1 1s ease-in-out infinite alternate;
+    -moz-animation: neon1 1s ease-in-out infinite alternate;
+    animation: neon1 1s ease-in-out infinite alternate;
+  }
+
+  .buttonB {
+    color: #fff;
+        -webkit-animation: neon2 1s ease-in-out infinite alternate;
+    -moz-animation: neon2 1s ease-in-out infinite alternate;
+    animation: neon2 1s ease-in-out infinite alternate;
+  }
+
+  .buttonP .buttonA:hover {
+    color: #45b6fe;
+    -webkit-animation: none;
+    -moz-animation: none;
+    animation: none;
+  }
+
+  .buttonP .buttonB:hover {
+    color: #FF1177;
+    -webkit-animation: none;
+    -moz-animation: none;
+    animation: none;
+  }
+
+  @-webkit-keyframes neon1 {
+    from {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #45b6fe, 0 0 70px #45b6fe, 0 0 80px #45b6fe, 0 0 100px #FF1177, 0 0 150px #FF1177;
+    }
+
+    to {
+      text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #45b6fe, 0 0 35px #45b6fe, 0 0 40px #45b6fe, 0 0 50px #FF1177, 0 0 75px #FF1177;
+    }
+  }
+
+    @-webkit-keyframes neon2 {
+    from {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #FF1177, 0 0 70px #FF1177, 0 0 80px #FF1177, 0 0 100px #FF1177, 0 0 150px #FF1177;
+    }
+
+    to {
+      text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #FF1177, 0 0 35px #FF1177, 0 0 40px #FF1177, 0 0 50px #FF1177, 0 0 75px #FF1177;
+    }
+  }
+
   #videoBG {
     position: fixed;
     right: 0;
@@ -55,7 +153,6 @@
   }
 
   .resultBackground {
-    /* background-image: url("../assets/avatarBackground.jpg"); */
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
@@ -68,7 +165,7 @@
   .resultDiv {
     position: absolute;
     height: 55%;
-    width: 60%;
+    width: 50%;
     left: 50%;
     top: 50%;
     border-radius: 10%;
@@ -78,24 +175,41 @@
     background-color: black;
   }
 
+  .resultUpper {
+    margin-top: 50px;
+    width: 100%;
+  }
+
+  .profileDiv {
+    display: inline-block;
+    width: 200px;
+    margin-left: 100px;
+  }
+
   .profileImg {
     height: 200px;
     width: 200px;
-    display: inline-block;
   }
 
   .userInfo {
     display: inline-block;
+    margin-left: 30px;
   }
 
-  .userInfo span {
+  .userInfo #userName {
     color: white;
     font-size: 4rem;
   }
 
+  .userInfo #performedTime {
+    color: white;
+    font-size: 1rem;
+  }
+
   .resultBottom {
-    height: 300px;
-    width: 600px;
+    height: 200px;
+    /* width: 600px; */
+    margin-left: 50px;
   }
 
   .starRatings {
@@ -106,18 +220,31 @@
     margin: 0 auto;
     position: relative;
     padding: 0;
-    border: 1px solid white;
+    /* border: 1px solid white; */
   }
 
   .scoreRatings {
     unicode-bidi: bidi-override;
     color: white;
-    font-size: 4rem;
+    /* font-size: 4rem; */
     display: inline-block;
-    margin: 0 auto;
+    /* margin: 0 auto; */
+    margin-left: 40px;
     position: relative;
     padding: 0;
-    border: 1px solid white;
+    /* border: 1px solid white; */
+  }
+
+  .rankingRatings {
+    unicode-bidi: bidi-override;
+    color: white;
+    /* font-size: 4rem; */
+    display: inline-block;
+    /* margin: 0 auto; */
+    margin-left: 40px;
+    position: relative;
+    padding: 0;
+    /* border: 1px solid white; */
   }
 
   .starRatingsTop {
@@ -129,13 +256,18 @@
     top: 0;
     left: 0;
     overflow: hidden;
-    animation: rating 3s forwards;
+    animation: rating 5s forwards;
   }
 
   .starRatingsBottom {
     padding: 0;
     display: block;
     z-index: 0;
+  }
+
+  .resultThird {
+    margin-left: 100px;
+    height: 100px;
   }
 
   @keyframes rating {
@@ -167,14 +299,5 @@
     font-style: normal;
     font-weight: 700;
     src: local('Open Sans Bold'), local('OpenSans-Bold'), url(https://fonts.gstatic.com/s/opensans/v17/mem5YaGs126MiZpBA-UN7rgOUuhs.ttf) format('truetype');
-  }
-
-  em {
-    font-style: italic;
-  }
-
-  h2 {
-    font-size: 16px;
-    margin-bottom: 15px;
   }
 </style>
