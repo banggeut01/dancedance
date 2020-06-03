072103d062f95fe9d363c36defcf1646b884260c
banggeut01 <genie121110@gmail.com>
2020-06-03 15:30:03 +0900

    S02P31B102-80 | Update avatar profile

--- frontend/src/components/rank/TotalRank.vue
+++ frontend/src/components/rank/TotalRank.vue
@@ -17,7 +17,7 @@
         </v-col>
         <v-col align="center" class="totalrank-width p-zero avatar-neon-div">
           <!-- 아바타 프로필 -->
-          <img src="@/assets/logo.png" style="height: 100%; width: 100%;">
+          <img :src="ranking[i - 1].img" style="height: 100%; width: 100%;">
         </v-col>
         <v-col class="p-zero rank-neon-border1" align="left">
           <!-- 이름 -->
@@ -50,7 +50,7 @@
         </v-col>
         <v-col align="center" class="totalrank-width p-zero avatar-neon-div">
           <!-- 아바타 프로필 -->
-          <img src="@/assets/logo.png" style="height: 100%; width: 100%;">
+          <img :src="myRanking.img" style="height: 100%; width: 100%;">
         </v-col>
         <v-col class="p-zero rank-neon-border1" align="left">
           <!-- 이름 -->
@@ -155,7 +155,7 @@
 .totalrank-text {
   line-height: 50px; 
   font-family: 'Black Han Sans', sans-serif; 
-  font-size: 25px; 
+  font-size: 20px; 
 }
 .skip-neon-div {
   border-style: solid;
