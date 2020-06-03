e74451e7d79ee179b9d9f81dfefa25eb033a5101
banggeut01 <genie121110@gmail.com>
2020-06-03 17:06:28 +0900

    S02P31B102-80 | Update neon btn

--- frontend/src/components/rank/SongRank.vue
+++ frontend/src/components/rank/SongRank.vue
@@ -2,8 +2,8 @@
   <div style="width:100%">
     <div class="slider-box">
         <div class="slider-list clearfix"></div>  
-        <button id="pree" @click="preSlide">preeee</button>
-        <button id="nextt" @click="nextSlide">nexxxxx</button>
+        <button id="pree" @click="preSlide"><i class="fas fa-caret-left fa-5x"></i></button>
+        <button id="nextt" @click="nextSlide"><i class="fas fa-caret-right fa-5x"></i></button>
     </div>
       <!-- {{ videoRanking }} -->
   </div>
@@ -21,6 +21,7 @@
     data() {
       return {
         path: [require('@/assets/introImg/scene00001.jpg'), require('@/assets/introImg/scene00002.jpg'), require('@/assets/introImg/scene00003.jpg')],
+        path1: [],
         curidx: 0,
       }
     },
@@ -63,30 +64,50 @@
     },
     mounted () {
       this.createImgList();
+      // for (let i=0 ; i < videoRanking.length ; i ++) {
+      //   path.appendChild(videoRanking.video[i].thumbnail)
+      // }
     },
 
   }
 </script>
 
 <style>
-  .slider-box {
-      position: relative;
-      overflow-x: hidden;
-      width: 80%;
-      padding-top: 40px;
-      margin: auto;
-    }
-    .clearfix:after {
-      content: ''; display: block; clear: both; float: none; 
-    }
-    .slider-box #pree { 
-      position: absolute;
-      top: 50%;
-      left: 0;
-    }
-    .slider-box #nextt {
-      position: absolute;
-      top: 50%;
-      right: 0;
-    }
+.slider-box {
+    position: relative;
+    overflow-x: hidden;
+    width: 80%;
+    padding-top: 40px;
+    margin: auto;
+  }
+.clearfix:after {
+  content: ''; display: block; clear: both; float: none; 
+}
+.slider-box #pree { 
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 10px;
+  color: #fff;
+  text-shadow: 0 0 10px #fff, 0 0 20px #228DFF, 0 0 30px #228DFF, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
+  border: 0;
+  outline: 0;
+}
+.slider-box #nextt {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 10px;
+  color: #fff;
+  text-shadow: 0 0 10px #fff, 0 0 20px #228DFF, 0 0 30px #228DFF, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
+  border: 0;
+  outline: 0;
+}
+.slider-list img {
+  /* background: linear-gradient(
+    to right,
+  rgba(255,255,255,0) 0%,
+  rgba(0,0,0,0.99) 66%,
+  rgba(0,0,0,1) 67%); */
+}
 </style>
