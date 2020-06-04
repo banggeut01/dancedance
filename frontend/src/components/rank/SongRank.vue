13f4749a7fef26ea7b8733bfa17cb7ec4adfd9e5
banggeut01 <genie121110@gmail.com>
2020-06-04 14:25:33 +0900

    S02P31B102-122 | Add Ranking Video Thimbnail

--- frontend/src/components/rank/SongRank.vue
+++ frontend/src/components/rank/SongRank.vue
@@ -5,6 +5,7 @@
         <button id="pree" @click="preSlide"><i class="fas fa-caret-left fa-5x"></i></button>
         <button id="nextt" @click="nextSlide"><i class="fas fa-caret-right fa-5x"></i></button>
     </div>
+    
       <!-- {{ videoRanking }} -->
   </div>
 </template>
@@ -20,8 +21,8 @@
     },
     data() {
       return {
-        path: [require('@/assets/introImg/scene00001.jpg'), require('@/assets/introImg/scene00002.jpg'), require('@/assets/introImg/scene00003.jpg')],
-        path1: [],
+        path1: [require('@/assets/introImg/scene00001.jpg'), require('@/assets/introImg/scene00002.jpg'), require('@/assets/introImg/scene00003.jpg')],
+        path: [],
         curidx: 0,
       }
     },
@@ -30,14 +31,96 @@
         const sliderList = document.querySelector('.slider-list')
         const sliderBox = document.querySelector('.slider-box')
         // const indexWidth = sliderBox.clientWidth
-        sliderList.style.width = `${100 * this.path.length}%`
+        sliderList.style.width = `${100 * this.videoRanking.length}%`
         // console.log(indexWidth)
-        for (let i=0; i<this.path.length; i ++) {
-          let img = new Image();
-          img.src = this.path[i]
-          img.style.width = `${100 / this.path.length}%`
-          img.style.cssFloat = 'left'
-          sliderList.appendChild(img)
+        // for (let i=0; i<this.path.length; i ++) {
+        for (let i=0; i<this.videoRanking.length; i ++) {
+          let img = new Image(); 
+          let div = document.createElement("div");
+          let h1 = document.createElement("h1")
+          let infodiv = document.createElement("div")
+          let singer = document.createElement("h3")
+          let difficulty = document.createElement("h3")
+          let singerDetail = document.createElement("h3")
+          let stardiv = document.createElement("div")
+          let itag = document.createElement("i")
+          let itag2 = document.createElement("i")
+          h1.innerText = this.videoRanking[i].video.title
+          singer.innerText = '가수'
+          difficulty.innerText = '난이도'
+          singerDetail.innerText = this.videoRanking[i].video.singer
+          img.src = this.videoRanking[i].video.thumbnail
+          itag.classList.add('fab')
+          itag.classList.add('fa-itunes-note')
+          itag.classList.add('fa-3x')
+          itag.classList.add('itag')
+          itag2.classList.add('fab')
+          itag2.classList.add('fa-itunes-note')
+          itag2.classList.add('fa-3x')
+          itag2.classList.add('itag2')
+
+          let tmp = ''
+          for (let i=0; i<this.videoRanking[i].video.difficulty; i++) {
+            tmp = tmp + '<i class="star-style fas fa-star fa-2x">'
+          }
+          stardiv.innerHTML = tmp
+
+          console.log(this.videoRanking[i].video.difficulty)
+          div.style.width = `${100 / this.videoRanking.length}%`
+          img.style.width = '100%'
+          infodiv.style.width = '100%'
+          // div 
+          div.style.cssFloat = 'left'
+          div.style.position = 'relative'
+          // video title
+          h1.style.position = 'absolute'
+          h1.style.top = '20%'
+          h1.classList.add("rankvideo-title")
+          h1.style.left = '50%'
+          h1.style.transform = 'translateX(-50%)'
+          // infodiv
+          infodiv.style.position = 'absolute'
+          infodiv.style.height = '40%'
+          infodiv.style.bottom = '4px'
+          infodiv.style.background = 'rgba(0, 0, 0, 0.8)'
+          // singer
+          singer.style.position = 'absolute'
+          singer.style.top = '20%'
+          singer.classList.add("rankvideo-info")
+          singer.style.right = '70%'
+          // difficulty
+          difficulty.style.position = 'absolute'
+          difficulty.style.bottom = '20%'
+          difficulty.classList.add("rankvideo-info")
+          difficulty.style.right = '70%'
+          // singerDetail
+          singerDetail.style.position = 'absolute'
+          singerDetail.style.top = '20%'
+          singerDetail.classList.add("singer-font")
+          singerDetail.style.left = '35%'
+          // stardiv
+          stardiv.style.position = 'absolute'
+          stardiv.style.display = "inline"
+          stardiv.style.bottom = '20%'
+          stardiv.style.left = '35%'
+          // itag
+          itag.style.position = 'absolute'
+          itag.style.right = '10%'
+          itag.style.top = '20%'
+          itag2.style.position = 'absolute'
+          itag2.style.right = '3%'
+          itag2.style.top = '40%'
+
+          infodiv.appendChild(stardiv)
+          infodiv.appendChild(singer)
+          infodiv.appendChild(difficulty)
+          infodiv.appendChild(singerDetail)
+          infodiv.appendChild(itag)
+          infodiv.appendChild(itag2)
+          div.appendChild(img)
+          div.appendChild(h1)
+          div.appendChild(infodiv)
+          sliderList.appendChild(div)
         }
       },
       preSlide() {
@@ -60,15 +143,28 @@
         
         sliderList.style.transition = "transform 0.4s ease-in-out";
         sliderList.style.transform = `translateX(${-(this.curidx) * 100/this.path.length}%)`;
+      },
+      setPath() {
+        for (let i=0 ; i < this.videoRanking.length ; i ++) {
+          this.path.push(this.videoRanking[i].video.thumbnail)
+        }
       }
     },
     mounted () {
-      this.createImgList();
-      // for (let i=0 ; i < videoRanking.length ; i ++) {
-      //   path.appendChild(videoRanking.video[i].thumbnail)
-      // }
+      // this.setPath()
+      // this.createImgList();
     },
-
+    watch: {
+      videoRanking: function() {
+        this.setPath()
+        this.createImgList();
+      }
+    },
+    computed: {
+      spath() {
+        return this.path
+      }
+    }
   }
 </script>
 
@@ -103,11 +199,45 @@
   border: 0;
   outline: 0;
 }
-.slider-list img {
-  /* background: linear-gradient(
-    to right,
-  rgba(255,255,255,0) 0%,
-  rgba(0,0,0,0.99) 66%,
-  rgba(0,0,0,1) 67%); */
+.rankvideo-title {
+  color: blue;
+  font-weight: 100;
+  font-size: 3em;
+  text-shadow: -1px -1px 0 #ffffff,
+    1px -1px 0 #ffffff,
+    -1px 1px 0 #ffffff,
+    1px 1px 0 #ffffff; 
+  font-family: 'Black Han Sans', sans-serif;
+}
+.rankvideo-info {
+  color: rgb(0, 255, 0);
+  font-weight: 100;
+  font-size: 2em;
+  text-shadow: -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000; 
+  font-family: 'Black Han Sans', sans-serif;
+}
+.singer-font {
+  color: rgb(255, 255, 255);
+  font-weight: 100;
+  font-size: 2em;
+  text-shadow: -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000; 
+  font-family: 'Black Han Sans', sans-serif;
+}
+.star-style {
+  color: yellow;
+}
+.itag {
+  color: blue;
+  text-shadow: 0 0 10px blue, 0 0 20px blue;
+}
+.itag2 {
+  color: purple;
+  text-shadow: 0 0 10px purple, 0 0 20px purple;
 }
 </style>
